e8e3fdf6f2e60bc910704b47fb165d5f4325acae
Edini Amare <edini.inbox@gmail.com>
2024-12-11 04:25:28 +0300

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,1 +1,11 @@
-create schema user_table
+-- create schema user_table;
+
+use user_table;
+
+describe user_table.tblusers;
+
+-- insert into tblusers (id, name, email, password)
+-- values
+-- (1,'edini','edini.inbox@gmail.com','pwd123');
+
+select * from user_table.tblusers;
